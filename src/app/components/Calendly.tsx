5902e6c0f4fb76da075ca11ed9faf0cb401d64c4
Jayden Pung <46307126+jaydenpung@users.noreply.github.com>
2024-02-25 18:06:19 +0800

    fix calendly height and add drag indicator

--- src/app/components/Calendly.tsx
+++ src/app/components/Calendly.tsx
@@ -3,11 +3,14 @@ import { Button, Modal } from '@mui/material';
 import React, { useEffect, useRef, useState } from 'react';
 import { PopupButton, PopupModal } from 'react-calendly';
 
-const Calendly = () => {
+interface Props {
+  rootRef: any;
+}
+const Calendly = ({ rootRef }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const ref = useRef(null);
+
   return (
-    <div ref={ref}>
+    <>
       <Button
         variant="contained"
         size="large"
@@ -22,20 +25,18 @@ const Calendly = () => {
         Request a 1-on-1 appointment now!
       </Button>
 
-      <div style={{ height: '70vh' }}>
-        <PopupModal
-          url="https://calendly.com/shenpo/shenpo-1-on-1"
-          onModalClose={() => setIsOpen(false)}
-          open={isOpen}
-          /*
-           * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-           * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-           */
-          //@ts-ignore
-          rootElement={ref.current}
-        />
-      </div>
-    </div>
+      <PopupModal
+        url="https://calendly.com/shenpo/shenpo-1-on-1"
+        onModalClose={() => setIsOpen(false)}
+        open={isOpen}
+        /*
+         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+         */
+        //@ts-ignore
+        rootElement={rootRef?.current}
+      />
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,26 @@
 import styles from './page.module.css';
 import Calendly from './components/Calendly';
 import Galaxy from './components/Galaxy';
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
+  const [beenHere, setBeenHere] = useState(true);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    localStorage.getItem('beenHere') ? setBeenHere(true) : setBeenHere(false);
+
+    setTimeout(() => {
+      setBeenHere(true);
+      localStorage.setItem('beenHere', 'true');
+    }, 3000);
+  }, []);
   return (
-    <div>
+    <div ref={ref}>
       {/* Calendly */}
       <div className={styles.calendlyContainer}>
-        <Calendly />
+        <Calendly rootRef={ref} />
       </div>
 
       {/* Galaxy */}
@@ -22,6 +35,20 @@ export default function Home() {
         <h1>神婆の占卦</h1>
         <h4>Plan your life based on your energy field</h4>
       </div>
+
+      {!beenHere && (
+        <Image
+          alt="drag-left"
+          src="/drag.gif"
+          width="80"
+          height="80"
+          style={{
+            position: 'fixed',
+            left: '50%',
+            bottom: '30%',
+          }}
+        />
+      )}
     </div>
   );
 }
